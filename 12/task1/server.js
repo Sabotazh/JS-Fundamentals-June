22b0ddb0144b184592300fac4b74a538248972fd
Sabotazh <sabotazh001@gmail.com>
2023-07-31 12:54:47 +0300

    Add homework for topic twelve

---
+++ 12/task1/server.js
@@ -0,0 +1,20 @@
+const http = require("http");
+const os = require("os");
+const path = require("path");
+
+http.createServer(function (request, response) {
+  response.writeHead(200, {'Content-Type': 'text/html'});
+
+  response.write("<h2>System information</h2>");
+  response.write("Current user name: " + os.userInfo().username);
+  response.write("<br>");
+  response.write("OS type: " + os.type());
+  response.write("<br>");
+  response.write("System work time: " + (os.uptime()/60).toFixed(2) + " minutes");
+  response.write("<br>");
+  response.write("Current work directory: " + path.dirname(__filename));
+  response.write("<br>");
+  response.end("Server file name: " + path.basename(__filename));
+}).listen(5000);
+
+console.log("Server is running on port :5000")
---
+++ 12/task2/personalmodule.js
@@ -0,0 +1,16 @@
+const currentDate = new Date();
+module.exports.date = currentDate;
+module.exports.showMessage = function (name) {
+  const hour = currentDate.getHours();
+
+  switch (true) {
+    case hour >= 5 && hour < 11:
+      return `Good morning, ${name}`;
+    case hour >= 11 && hour < 17:
+      return `Good day, ${name}`;
+    case hour >= 17 && hour < 23:
+      return `Good evening, ${name}`;
+    case hour >= 23 && hour < 5:
+      return `Goodnight, ${name}`;
+  }
+};
